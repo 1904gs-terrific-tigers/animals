dd754f7e345d26076e5ae316de4cfdfe8ab231c8
Manuel Bugallo <marvinody@gmail.com>
2019-06-11 15:26:34 -0400

    updated single animal component

--- client/components/single-animal.js
+++ client/components/single-animal.js
@@ -4,7 +4,30 @@ import React from 'react'
 //component
 
 export const SingleAnimal = props => {
-  return <div />
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h3>{props.species}</h3>
+      <img src={props.imageUrl} />
+      <div className="desc">
+        <p>{props.description}</p>
+      </div>
+      <div className="pricing">
+        <p>
+          ${props.pricePerTimeUnit} / {props.timeUnit} min
+        </p>
+      </div>
+      <div className="quantity">
+        <span>
+          <label htmlFor="quantity">Quantity:</label>
+        </span>
+        <span>
+          <input id="quantity" name="quantity" value="1" type="number" />
+        </span>
+      </div>
+      <button type="submit">Add to Cart</button>
+    </div>
+  )
 }
 
 SingleAnimal.propTypes = {
