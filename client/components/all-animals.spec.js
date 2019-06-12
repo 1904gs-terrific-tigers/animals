154707f547b15d605b623bdc28911536db896058
Manuel Bugallo <marvinody@gmail.com>
2019-06-12 08:56:32 -0400

    updated all-animals component tests

--- client/components/all-animals.spec.js
+++ client/components/all-animals.spec.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-expressions */
 import {expect} from 'chai'
-import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
 import {AllAnimals} from './all-animals'
 
 const adapter = new Adapter()
@@ -15,6 +16,13 @@ let animalsData = [
       'https://media.treehugger.com/assets/images/2018/03/sloth-sounds.jpg.860x0_q70_crop-scale.jpg',
     species: 'sloth',
     pricePerTimeUnit: 1
+  },
+  {
+    id: 2,
+    name: 'Spend time with catboat',
+    imageUrl: 'http://catbo.at/catboat.jpg',
+    species: 'catboat',
+    pricePerTimeUnit: 100
   }
 ]
 
@@ -27,11 +35,43 @@ describe('AllAnimals', () => {
     )
   })
 
+  it('renders with a key prop', () => {
+    expect(
+      animals
+        .find('div > div')
+        .first()
+        .key()
+    ).to.exist
+  })
+
+  it('renders the correct amount of animals', () => {
+    expect(animals.find('h2')).to.have.lengthOf(animalsData.length)
+  })
+
   it('renders an animal name in an h2', () => {
-    expect(animals.find('h2').text()).to.be.equal('Cody')
+    expect(
+      animals
+        .find('h2')
+        .first()
+        .text()
+    ).to.be.equal(animalsData[0].name)
+  })
+
+  it("renders the animal's image", () => {
+    expect(
+      animals
+        .find('img')
+        .first()
+        .prop('src')
+    ).to.be.equal(animalsData[0].imageUrl)
   })
 
   it('renders an animal price in an h3', () => {
-    expect(animals.find('h3').text()).to.be.equal('Price: 1')
+    expect(
+      animals
+        .find('h3')
+        .first()
+        .text()
+    ).to.contain(animalsData[0].pricePerTimeUnit)
   })
 })
