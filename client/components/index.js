40a3d44f1f108fcd0195951852c24084134a6806
Manuel Bugallo <marvinody@gmail.com>
2019-06-12 09:03:00 -0400

    changed export to be consistent

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,8 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
-export {Login, Signup} from './auth-form'
 export {default as AllAnimals} from './all-animals'
+export {Login, Signup} from './auth-form'
+export {default as Navbar} from './navbar'
+export {default as SingleAnimal} from './single-animal'
+export {default as UserHome} from './user-home'
