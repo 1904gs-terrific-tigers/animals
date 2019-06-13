2ccab7f15a8119dc8b54bb8670bedc647540f402
Manuel Bugallo <marvinody@gmail.com>
2019-06-13 08:26:14 -0400

    refactor(routes/cart.spec) changed structure to allow for extension

--- server/api/cart.spec.js
+++ server/api/cart.spec.js
@@ -8,23 +8,23 @@ const app = require('../index')
 const {User, Order, AnimalOrder, Animal} = require('../db/models')
 
 describe('Cart routes', () => {
-  let dummyUser, dummyOrder, dummyAnimal
+  let dummyUser, dummyOrder, cody
+
+  let userSignIn = {
+    email: 'cody@puppybook.com',
+    password: '123'
+  }
 
   beforeEach(async () => {
     await db.sync({force: true})
 
-    const codysEmail = 'cody@puppybook.com'
-
-    dummyUser = await User.create({
-      email: codysEmail,
-      password: '123'
-    })
+    dummyUser = await User.create(userSignIn)
 
     dummyOrder = await dummyUser.createOrder({
       purchased: false
     })
 
-    dummyAnimal = await Animal.create({
+    cody = await Animal.create({
       name: 'Cody',
       species: 'sloth',
       price: 100
@@ -33,10 +33,10 @@ describe('Cart routes', () => {
 
   describe('/api/cart/', () => {
     beforeEach(async () => {
-      await dummyOrder.addAnimal(dummyAnimal, {
+      await dummyOrder.addAnimal(cody, {
         through: {
           quantity: 10,
-          price: dummyAnimal.price
+          price: cody.price
         }
       })
     })
@@ -44,17 +44,15 @@ describe('Cart routes', () => {
     it('GET /api/cart', async () => {
       const agent = request.agent(app)
 
-      await agent.post('/auth/login').send({
-        email: 'cody@puppybook.com',
-        password: '123'
-      })
+      await agent.post('/auth/login').send(userSignIn)
+
       const res = await agent.get('/api/cart').expect(200)
       expect(res.body).to.be.an('array')
-      expect(res.body[0].id).to.equal(dummyAnimal.id) //first elem in array is animal obj
-      expect(res.body[0].name).to.equal(dummyAnimal.name)
-      expect(res.body[0].imageUrl).to.equal(dummyAnimal.imageUrl)
-      expect(res.body[0].timeUnit).to.equal(dummyAnimal.timeUnit)
-      expect(res.body[0].price).to.equal(dummyAnimal.price)
+      expect(res.body[0].id).to.equal(cody.id) //first elem in array is animal obj
+      expect(res.body[0].name).to.equal(cody.name)
+      expect(res.body[0].imageUrl).to.equal(cody.imageUrl)
+      expect(res.body[0].timeUnit).to.equal(cody.timeUnit)
+      expect(res.body[0].price).to.equal(cody.price)
       expect(res.body[0].quantity).to.equal(10)
     })
 
@@ -63,10 +61,7 @@ describe('Cart routes', () => {
       // if it exists and is not empty (no items inside cart), set bought to true. do nothing otherwise.
       const agent = request.agent(app)
 
-      await agent.post('/auth/login').send({
-        email: 'cody@puppybook.com',
-        password: '123'
-      })
+      await agent.post('/auth/login').send(userSignIn)
 
       expect(dummyOrder.purchased).to.be.false
 
@@ -75,6 +70,10 @@ describe('Cart routes', () => {
       const updatedOrder = await Order.findOne({id: dummyOrder.id})
 
       expect(updatedOrder.purchased).to.be.true
+    })
+
+    describe('POST /api/cart/:animalId', () => {
+      it('should set it to given amount if the cart does not have the item in the cart currently', () => {})
     })
   }) // end describe('/api/cart')
 }) // end describe('Car routes')
