9bd5b4a6b69759e26985e812d6ccb16c76109023
Justin Kichler <jkichler@gmail.com>
2019-06-18 19:13:28 -0400

    feat(app) added a theme to the store

--- client/app.js
+++ client/app.js
@@ -3,12 +3,31 @@ import React from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
 
+import {createMuiTheme, withStyles} from '@material-ui/core/styles'
+import {ThemeProvider} from '@material-ui/styles'
+import purple from '@material-ui/core/colors/purple'
+import green from '@material-ui/core/colors/green'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#29316b'
+    },
+    secondary: purple
+  },
+  status: {
+    danger: 'orange'
+  }
+})
+
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    </ThemeProvider>
   )
 }
 
